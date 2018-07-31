c252e86274a9143e3d71a5c0ce0ab69c1c69cc34
Zach C <FxChiP@Gmail.com>
2018-07-31 02:35:20 -0700

    Add a decorator to not require operative functions to do their own currying

--- prymer/op.py
+++ prymer/op.py
@@ -2,30 +2,39 @@
 __all__ = ["FromAttr", "KeyOrDefault", "Template"]
 
 
-def FromAttr(attr_name, *default):
-    def impl(source):
-        return getattr(source, attr_name, *default)
-    return impl
+def Composite(func):
+    # Take the parameters
+    def param_collections(*args, **kwargs):
+        # Take the data to apply to
+        def source_data(source):
+            # Call the composite with the source
+            # data and the parameters
+            return func(source, *args, **kwargs)
+        return source_data
+    return param_collections
 
 
-def KeyOrDefault(key_name, *default):
-    def impl(source):
-        try:
-            return source[key_name]
-        except (KeyError, IndexError) as e:
-            if not default:
-                raise
-            else:
-                return default[0]
-    return impl
+@Composite
+def FromAttr(source, attr_name, *default):
+    return getattr(source, attr_name, *default)
 
 
-def Template(template, resolver=None):
-    def impl(source):
-        if callable(resolver):
-            source = resolver(source)
-        if isinstance(source, dict):
-            return template.format(**source)
+@Composite
+def KeyOrDefault(source, key_name, *default):
+    try:
+        return source[key_name]
+    except (KeyError, IndexError) as e:
+        if not default:
+            raise
         else:
-            return template.format(*source)
-    return impl
+            return default[0]
+
+
+@Composite
+def Template(source, template, resolver=None):
+    if callable(resolver):
+        source = resolver(source)
+    if isinstance(source, dict):
+        return template.format(**source)
+    else:
+        return template.format(*source)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="prymer",
-    version="0.0.1",
+    version="0.0.2",
     author="Zach Carlson",
     author_email="FxChiP@Gmail.com",
     description="prymer: Python-native data templating",
