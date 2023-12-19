a5875074659ee947cdb90d247bce868aed8482c0
Zach Carlson <fxchip+git@gmail.com>
2023-12-18 22:58:01 -0600

    Quick test for nested coroutines

--- prymer_async_test.py
+++ prymer_async_test.py
@@ -8,14 +8,18 @@ async def get_IP(_):
     async with aiohttp.ClientSession() as session:
         async with session.get('http://canhazip.com') as resp:
             return await resp.text()
+        
+async def nested_get_IP(_):
+    ip = await get_IP(_)
+    return {ip: {"ip": get_IP}}
 
 async def main(): 
-    my_IP = prymer.port({}, {"my_IP": get_IP})
+    my_IP = prymer.port({}, {"my_IP": nested_get_IP})
     print(f"My IP raw {my_IP}")
     r_IP = await my_IP
     print(f"My IP resolved {r_IP}")
 
-    my_IP2 = prymer.port({}, [0, 2, get_IP, 5, "beach"])
+    my_IP2 = prymer.port({}, [0, 2, nested_get_IP, 5, "beach"])
     print(f"test again raw {my_IP2}")
     r_IP = await my_IP2
     print(f"test again resolved {r_IP}")
